60bf2645ddea1e56996d5c5e8b5afb7f5722753d
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2020-06-20 13:07:07 -0600

    Add files via upload

---
+++ Not/displayValues.php
@@ -0,0 +1,45 @@
+<?php
+
+$fristName = 'Dayn';
+$number = 49.99;
+$lastName = 'Augustson';
+
+echo $fristName; 
+echo ' ';
+echo '"The Bad Ass"';
+echo ' ';
+echo $lastName;
+?>
+<br>
+
+<?php
+print $fristName;
+print $number;
+?>
+<br>
+
+<?php
+
+echo $number, ' ', 'What is this',' ',$lastName , ' ', $fristName;
+
+?>
+<br><br>
+
+<?php
+
+$book = '"Hitchhker\'s guid to the Galxey"'; 
+//echo $book;
+echo "I love $book";
+?>
+<br><br>
+
+<?php
+
+$firstName = 'Daivd';
+$lastName = 'Powers';
+$title = '"Hitchhker\'s guid to the Galxey"';
+$author = 'Douglas Adams';
+$answer = 42;
+$newLines = "\r\n\r\n";
+?>
+
